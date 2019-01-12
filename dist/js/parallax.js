9f4596b19d0d3c4ca3cdf8d24a44e8e83328ca63
Aaron Buachie <aaron.buachiea@gmail.com>
2019-01-12 16:00:32 -0500

    fixed column sections and added contact section

--- dist/js/parallax.js
+++ dist/js/parallax.js
@@ -1,5 +1,12 @@
 $(window).scroll(function () {
     let scrollTop = $(this).scrollTop();
-
+    /*
+    while ($("#services").scrollTop() == 0) {
+        $("#services").css("background - position - y", -(scrollTop * 0.5) + "px");
+    }
+    while ($("#header").scrollTop(0) == 0) {
+        $("#header").css("background - position - y", -(scrollTop * 0.5) + "px");
+    }
+    */
     $("#header, #services").css("background-position-y", -(scrollTop * 0.5) + "px");
 })
